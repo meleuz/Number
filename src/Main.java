568258f6a83fcbd3979d89faf9066b5cc4f51021
Aigul Biktasheva <meleuz13@gmail.com>
2021-03-08 11:55:27 +0500

    number ADDED

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "36580995331930";
+        String number = "5351719427810741";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
